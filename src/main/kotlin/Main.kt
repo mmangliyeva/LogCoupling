f523bebe4423691325f21b16ea64eaaa71ac7f11
Mehriniso Mangliyeva <m.mangliyeva@tum.de>
2024-05-02 15:10:14 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,81 @@
 import khttp.get
 import org.json.JSONArray
 
-fun main() {
-    val repoOwner = "owner"
-    val repoName = "repository"
-    val token = "your_github_token"
+fun main(args: Array<String>) {
 
+    if (args.size != 3) {
+        println("Usage: kotlin MainKt <owner> <repository> <token>")
+        return
+    }
+
+    val repoOwner = args[0]
+    val repoName = args[1]
+    val token = args[2] 
+
+    
     val commitsUrl = "https://api.github.com/repos/$repoOwner/$repoName/commits"
     val commitsResponse = get(commitsUrl, headers = mapOf("Authorization" to "token $token"))
     val commits = commitsResponse.jsonArray
 
-    // Map to store files/modules and their associated contributors
+    
+    var totalCommits = 0
+    val uniqueContributors = mutableSetOf<String>()
+    val fileModificationCount = mutableMapOf<String, Int>()
+
+    
+    for (commit in commits) {
+        totalCommits++
+        val commitUrl = commit["url"] as String
+        val commitResponse = get(commitUrl, headers = mapOf("Authorization" to "token $token"))
+        val files = (commitResponse.jsonObject["files"] as JSONArray)
+        val contributor = (commitResponse.jsonObject["author"] as Map<String, Any?>)["login"] as String
+        uniqueContributors.add(contributor)
+
+        
+        for (file in files) {
+            val filename = (file as Map<String, Any?>)["filename"] as String
+            fileModificationCount[filename] = fileModificationCount.getOrDefault(filename, 0) + 1
+        }
+    }
+
+    
+    val mostModifiedFiles = fileModificationCount.entries.sortedByDescending { it.value }.take(5)
+
+    
+    println("Repository Analysis for $repoOwner/$repoName:")
+    println("Total Commits: $totalCommits")
+    println("Unique Contributors: ${uniqueContributors.size}")
+    println("Most Modified Files/Modules:")
+    mostModifiedFiles.forEach { (file, count) ->
+        println("$file: $count modifications")
+    }
+
+    
+    val developerPairsFrequency = mutableMapOf<Pair<String, String>, Int>()
     val fileContributorsMap = mutableMapOf<String, MutableSet<String>>()
 
-    // Iterate over commits
+    
     for (commit in commits) {
         val commitUrl = commit["url"] as String
         val commitResponse = get(commitUrl, headers = mapOf("Authorization" to "token $token"))
         val files = (commitResponse.jsonObject["files"] as JSONArray)
+        val contributors = (commitResponse.jsonObject["author"] as Map<String, Any?>)["login"] as String
 
-        val contributor = (commitResponse.jsonObject["author"] as Map<String, Any?>)["login"] as String
-
-        // Update fileContributorsMap for each modified file/module
+    
         for (file in files) {
             val filename = (file as Map<String, Any?>)["filename"] as String
 
             if (!fileContributorsMap.containsKey(filename)) {
-                fileContributorsMap[filename] = mutableSetOf(contributor)
+                fileContributorsMap[filename] = mutableSetOf(contributors)
             } else {
-                fileContributorsMap[filename]?.add(contributor)
+                fileContributorsMap[filename]?.add(contributors)
             }
         }
     }
 
-    // Calculate frequency of contribution by pairs of developers to the same files/modules
-    val developerPairsFrequency = mutableMapOf<Pair<String, String>, Int>()
-
+    
     for ((_, contributors) in fileContributorsMap) {
-        // Create pairs of contributors for each file/module
+        
         val contributorList = contributors.toList()
         for (i in 0 until contributorList.size - 1) {
             for (j in i + 1 until contributorList.size) {
@@ -46,9 +84,8 @@ fun main() {
             }
         }
     }
-
-    // Display frequency of contribution by pairs of developers to the same files/modules
-    println("Frequency of contribution by pairs of developers to the same files/modules:")
+    
+    println("\nFrequency of contribution by pairs of developers to the same files/modules:")
     developerPairsFrequency.forEach { (pair, frequency) ->
         println("$pair: $frequency times")
     }
